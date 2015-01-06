0813343e3f927005859490c62e273b156c5ee835
Faci, Samir <dev@esamir.com>
2015-01-06 00:36:13 -0800

    Pelican Site

---
+++ content/first_post.md
@@ -0,0 +1,5 @@
+Title: My First Review
+Date: 2010-12-03 10:20
+Category: Greeting
+
+This is a required hello world top post.
---
+++ pelicanconf.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'pingus'
+SITENAME = u'OSAlt Site'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# working
+# Blogroll
+LINKS = (('NixiePixel', 'http://www.nixiepixel.com'),
+                 #('Python.org', 'http://python.org/'),
+                 #         ('Jinja2', 'http://jinja.pocoo.org/')
+                          )
+
+SOCIAL = (('twitter', 'http://twitter.com/NixiePixel'),
+                  ('github', 'https://github.com/OSAlt'),
+                           )
+
+DEFAULT_PAGINATION = 10
+
+THEME = "pelican-themes/water-iris"
+
+
